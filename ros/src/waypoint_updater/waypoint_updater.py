6bbaea68ed687c48d364de98983e07d4279e2e78
BScottHenderson <henders999@gmail.com>
2019-04-21 15:36:38 -0700

    Adjust waypoint speed before publishing.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -28,7 +28,7 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
 MAX_DECEL     = 0.5 # Deceleration limit.
-MAX_SPEED_METERS_PER_SEC = 10*0.447 # 10 mph
+MAX_SPEED_METERS_PER_SEC = 100*0.447 # 10 mph
 
 
 class WaypointUpdater(object):
