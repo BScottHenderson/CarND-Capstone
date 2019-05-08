62fd87cd4d13df5dc0df3ca343b164abbe2f9c10
BScottHenderson <henders999@gmail.com>
2019-05-08 07:52:39 -0700

    Disable the image callback.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -96,6 +96,8 @@ class TLDetector(object):
         Args:
             msg (Image): image from car-mounted camera
         """
+        rospy.logwarn('tl_detector: image_cb: Received image.')
+        return
         self.camera_image = msg
         light_wp, state   = self.process_traffic_lights()
 
